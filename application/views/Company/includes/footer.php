f6ec128fa228adc8389bf14c2aea94a6e3bf28fe
digiblade <ak.chourasia.007@gmail.com>
2021-04-18 23:40:24 +0530

    intial commit

---
+++ application/views/Company/includes/footer.php
@@ -0,0 +1,23 @@
+
+  <!-- Main Footer -->
+  <footer class="main-footer">
+    <!-- To the right -->
+    <div class="float-right d-none d-sm-inline">
+      Anything you want
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2019- <?php echo date("Y");?> <a href="https://digiblade.in">Digiblade.in</a>.</strong> All rights reserved.
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- REQUIRED SCRIPTS -->
+
+<!-- jQuery -->
+<script src="<?php echo base_url();?>plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?php echo base_url();?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?php echo base_url();?>dist/js/adminlte.min.js"></script>
+</body>
+</html>
